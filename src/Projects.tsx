ee3b2c8bddebdad341de5656d510d67937c74d64
dsnjg03 <domagojsenjug03@gmail.com>
2026-04-16 15:55:35 +0200

    Wording change for Projects page

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -18,7 +18,7 @@ export default function Projects(){
             <div className="relative w-full flex justify-center items-center mt-4">
                 <div className="xl:mx-40 mx-20 select-none pointer-events-none bg-primary/10 flex flex-col justify-center p-3 border-2 border-primary/20 rounded-2xl">
                     <h1 className="text-bright text-5xl font-display font-bold text-center">PROJECTS</h1>
-                    <p className="text-muted font-display font-semibold text-center lg:text-left">Projects I have worked on, or am working on.
+                    <p className="text-muted font-display font-semibold text-center lg:text-left">Some of the projects I have worked on, or am working on.
                         Click on a project to view more information.
                     </p>
                 </div>
